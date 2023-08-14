c91df5095335c5601b65794ec21cb1cc3e16cd90
XueqianDeng <61855837+XueqianDeng@users.noreply.github.com>
2023-08-14 19:48:36 -0400

    Update Aug_14_Hokin_Collection.py

--- Standard_Version/Aug_14_Hokin_Collection.py
+++ Standard_Version/Aug_14_Hokin_Collection.py
@@ -183,7 +183,7 @@ async def experiment():
 
             # Save Rest Data
             rest_csv_data_path = data_path + "/Section_Number_" + str(current_section) + "rest.csv"
-            df = pd.DataFrame(data = close_data_holder, columns = ['C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'C10', 'C11', 'C12',
+            df = pd.DataFrame(data = rest_data_holder, columns = ['C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'C10', 'C11', 'C12',
                                 'C13', 'C14', 'C15', 'C16', 'Instruction', 'Signal_Time', 'Batch_time','X','Y'])
             df.to_csv(rest_csv_data_path,mode='a',header=False, index=False)
 
